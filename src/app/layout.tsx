1dd42f873be47043554ecf1641c6ed2dd967abd4
Haitham Athamneh <alathamneh.haitham@gmail.com>
2025-04-07 10:12:01 +0200

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
 			>
 				{children}
 			</body>
+			<Analytics />
 			<Script
 				src="https://challenges.cloudflare.com/turnstile/v0/api.js"
 				async
